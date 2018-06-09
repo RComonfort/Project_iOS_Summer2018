0df5f4f1fb1968df9bf7bfd976ae6ba305389edf
R. Comonfort <a.comonfort@outlook.com>
2018-06-09 17:54:12 -0500

    more layout

--- ExpensesTracker/StatusViewController.swift
+++ ExpensesTracker/StatusViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class StatusViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,25 @@ class StatusViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onAddIncomeExpense(_ sender: UIButton) {
+        
+        //If the user wants to add an income
+        if (sender.tag == 0)
+        {
+            
+        } //Else if the user wants to add an expense
+        else if (sender.tag == 1)
+        {
+            
+        }
+        else {
+            fatalError("Not a valid sender");
+        }
+        
+        performSegue(withIdentifier: "IncomeExpenseSegue", sender: self)
+        
+    }
+    
 
 }
 
