5d396f3a94ae0c8f3f2af5aa75af6600fc027496
Alumno <alumno@imac-de-itesm-2.local>
2018-06-15 12:11:58 -0500

    Created and started using the ReportViewController File. Added the necessary methods for the picker view.

---
+++ ExpensesTracker/ReportViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ReportViewController.swift
+//  ExpensesTracker
+//
+//  Created by Alumno on 15/06/18.
+//  Copyright © 2018 Comonfort. All rights reserved.
+//
+
+import UIKit
+
+class ReportViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var percentage: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let amount = ["Total", "Budget"]
+    let timeInterval = DefaultData.getTimeIntervals()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0{
+            return amount.count
+        }
+        else{
+            return timeInterval.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0{
+            return amount[row]
+        }
+        else{
+            return timeInterval[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
+    }
+    
+    func getBudgetInfo(timeIntervalIndex: Int){
+        
+    }
+    
+    func getTotalInfo(timeIntervalIndex: Int){
+        
+    }
+    
+}
