7ecae8a87f5ac59ed5f1b55b31060d451e0a800e
Alumno <alumno@imac-de-itesm-2.local>
2018-06-28 09:48:25 -0500

    Changed timer so it repeats

--- ExpensesTracker/IncomeExpenseViewController.swift
+++ ExpensesTracker/IncomeExpenseViewController.swift
@@ -235,7 +235,7 @@ class IncomeExpenseViewController: UIViewController, UIPickerViewDelegate, UIPic
             components.second = 0
             let date = Calendar.current.date(from: components)
             print(date!)
-            let timer = Timer(fireAt: date!, interval: Double(DefaultData.getTimeIntervalsSeconds()[0]), target: self, selector: #selector(runCode), userInfo: nil, repeats: false)
+            let timer = Timer(fireAt: date!, interval: Double(86400), target: self, selector: #selector(runCode), userInfo: nil, repeats: true)
             RunLoop.main.add(timer, forMode: RunLoopMode.commonModes)
             return
         } else {
